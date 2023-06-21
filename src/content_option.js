995ab4baf524d28eff0bf0468bc20bfe2b998f36
awinton64 <awinton21@gmail.com>
2023-06-21 01:14:38 -0500

    updated captureitall png and portfolio layout

--- src/content_option.js
+++ src/content_option.js
@@ -22,99 +22,139 @@ const dataabout = {
     aboutme: "I believe the best moments in life are those we create together. Whether it's playing games like foosball or having conversations over a shared meal, I find the most enjoyment in these experiences. I also have a passion for building physical products, whether it's a Frankenstein-inspired electric scooter or a web app designed to bring friends closer. I truly love the process of building and creating. If given the opportunity, I look forward to getting to know you too.",
 };
 const worktimeline = [{
-        jobtitle: "Product Manager",
-        where: "West Monroe",
-        date: "Aug 2022 – June 2023",
-    },
-    {
-        jobtitle: "Business Analyst",
-        where: "Frenik Marketing Group",
-        date: "May 2021 – August 2021",
-    },
-    {
-        jobtitle: "Co-founder",
-        where: "Atlanta, GA",
-        date: "March 2020 – December 2020",
-    },
+    jobtitle: "Product Manager",
+    where: "West Monroe",
+    date: "Aug 2022 – June 2023",
+},
+{
+    jobtitle: "Business Analyst",
+    where: "Frenik Marketing Group",
+    date: "May 2021 – August 2021",
+},
+{
+    jobtitle: "Co-founder",
+    where: "Atlanta, GA",
+    date: "March 2020 – December 2020",
+},
 ];
 
 const skills = [{
-        name: "Agile ceremonies",
-        value: 90,
-    },
-    {
-        name: "Atlassian tools (Jira, Confluence)",
-        value: 85,
-    },
-    {
-        name: "Figma",
-        value: 80,
-    },
-    {
-        name: "Adobe XD",
-        value: 80,
-    },
-    {
-        name: "Miro",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 70,
-    },
-    {
-        name: "JavaScript",
-        value: 70,
-    },
-    {
-        name: "HTML",
-        value: 70,
-    },
-    {
-        name: "CSS",
-        value: 70,
-    },
-    {
-        name: "Django",
-        value: 20,
-    }
+    name: "Agile ceremonies",
+    value: 90,
+},
+{
+    name: "Atlassian tools (Jira, Confluence)",
+    value: 85,
+},
+{
+    name: "Figma",
+    value: 80,
+},
+{
+    name: "Adobe XD",
+    value: 80,
+},
+{
+    name: "Miro",
+    value: 80,
+},
+{
+    name: "React",
+    value: 70,
+},
+{
+    name: "JavaScript",
+    value: 70,
+},
+{
+    name: "HTML",
+    value: 70,
+},
+{
+    name: "CSS",
+    value: 70,
+},
+{
+    name: "Django",
+    value: 20,
+}
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+    title: "UI & UX Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Mobile Apps",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Wordpress Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
 ];
 
 const dataportfolio = [{
-        img: captureitall,
-        description: "This is a demo of our user-friendly web app for audio capture. CaptureItAll is an audio capture service designed to enhance the wedding experience for couples and guests. Our goal is to capture and document cherished moments and memories throughout the event, from the guest's perspective. The intended outcome is a custom vinyl record that encapsulates one of life's greatest shared moments.",
-        link: "https://captureitall.netlify.app/",
+    title: "Capture It All",
+    animated: {
+        first: "Create together",
+        second: "Captured together",
+        third: "Cherished forever",
     },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+    img: captureitall,
+    description: "This is a demo of our user-friendly web app for audio capture. CaptureItAll is an audio capture service designed to enhance the wedding experience for couples and guests. Our goal is to capture and document cherished moments and memories throughout the event, from the guest's perspective. The intended outcome is a custom vinyl record that encapsulates one of life's greatest shared moments.",
+    link: "https://captureitall.netlify.app/",
+},
+{
+    title: "SpotMe's Lift Finder",
+    animated: {
+        first: "Need a spotter?",
+        second: "Easy find a spot",
+        third: "Use lift finder",
     },
-    {
-        img: "https://picsum.photos/400/800?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    title: "SpotMe's Group Profile",
+    animated: {
+        first: "Find friends",
+        second: "Know when they are working out",
+        third: "Compete & celebarte together",
     },
+    img: "https://picsum.photos/400/800?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    title: "SpotMe's Personal Profile",
+    animated: {
+        first: "Create and track workouts",
+        second: "Measure your progress",
+        third: "Achieve your goals",
+    },
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    title: "Compost2Go",
+    animated: {
+        first: "Contribute to a zero waste world",
+        second: "Participate through your apartment",
+        third: "Weekly pickups",
+    },
+    img: "https://picsum.photos/400/800?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
 ];
 
 const contactConfig = {
     YOUR_EMAIL: "andrewwinton909@gmail.com",
     YOUR_FONE: "(678)408-1063",
-    description: "Please give me a call or send me an email. I look forward to getting to know you.", 
+    description: "Please give me a call or send me an email. I look forward to getting to know you.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_g458g1w",
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
-import { dataportfolio, meta } from "../../content_option";
+import { meta, dataportfolio, introdata } from "../../content_option";
+import Typewriter from "typewriter-effect";
+
 
 export const Portfolio = () => {
   return (
@@ -10,28 +12,171 @@ export const Portfolio = () => {
       <Container className="About-header">
         <Helmet>
           <meta charSet="utf-8" />
-          <title> Portfolio | {meta.title} </title>{" "}
+          <title>Portfolio | {meta.title}</title>
           <meta name="description" content={meta.description} />
         </Helmet>
         <Row className="mb-5 mt-3 pt-md-3">
           <Col lg="8">
-            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
+            <h1 className="display-4 mb-4">Portfolio</h1>
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
         </Row>
+
         <div className="mb-5 po_items_ho">
-          {dataportfolio.map((data, i) => {
-            return (
-              <div key={i} className="po_item">
-                <img src={data.img} alt="" />
-                <div className="content">
-                  <p>{data.description}</p>
-                  <a href={data.link}>view project</a>
-                </div>
+          <Row>
+            <Col lg="6" className="d-flex align-items-center justify-content-center">
+              <div className="po_item">
+                <img src={dataportfolio[0].img} alt="" className="portfolio-image" />
               </div>
-            );
-          })}
+            </Col>
+            <Col lg="6" className="text-center align-self-center">
+              <div className="content text-left">
+              <h2 className="mb-1x">{dataportfolio[0].title}</h2>
+                <h1 className="fluidz-48 mb-1x">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        dataportfolio[0].animated.first,
+                        dataportfolio[0].animated.second,
+                        dataportfolio[0].animated.third,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 40,
+                    }}
+                  />
+                </h1>
+                <p>{dataportfolio[0].description}</p>
+                <a href={dataportfolio[0].link}>view demo</a>
+              </div>
+            </Col>
+          </Row>
         </div>
+        <hr className="t_border my-4 ml-0 text-left" />
+        <br/>
+        <div className="mb-5 po_items_ho">
+          <Row>
+            <Col lg="6" className="text-center align-self-center">
+              <div className="content text-left">
+              <h2 className="mb-1x">{dataportfolio[1].title}</h2>
+                <h1 className="fluidz-48 mb-1x">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        dataportfolio[1].animated.first,
+                        dataportfolio[1].animated.second,
+                        dataportfolio[1].animated.third,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 40,
+                    }}
+                  />
+                </h1>
+                <p>{dataportfolio[1].description}</p>
+                <a href={dataportfolio[1].link}>view project</a>
+              </div>
+            </Col>
+            <Col lg="6" className="d-flex align-items-center justify-content-center">
+              <div className="po_item">
+                <img src={dataportfolio[1].img} alt="" className="portfolio-image" />
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <div className="mb-5 po_items_ho">
+          <Row>
+            <Col lg="6" className="d-flex align-items-center justify-content-center">
+              <div className="po_item">
+                <img src={dataportfolio[2].img} alt="" className="portfolio-image" />
+              </div>
+            </Col>
+            <Col lg="6" className="text-center align-self-center">
+              <div className="content text-left">
+              <h2 className="mb-1x">{dataportfolio[2].title}</h2>
+                <h1 className="fluidz-48 mb-1x">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        dataportfolio[2].animated.first,
+                        dataportfolio[2].animated.second,
+                        dataportfolio[2].animated.third,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 40,
+                    }}
+                  />
+                </h1>
+                <p>{dataportfolio[2].description}</p>
+                <a href={dataportfolio[2].link}>view project</a>
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <div className="mb-5 po_items_ho">
+          <Row>
+            <Col lg="6" className="text-center align-self-center">
+              <div className="content text-left">
+              <h2 className="mb-1x">{dataportfolio[3].title}</h2>
+                <h1 className="fluidz-48 mb-1x">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        dataportfolio[3].animated.first,
+                        dataportfolio[3].animated.second,
+                        dataportfolio[3].animated.third,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 40,
+                    }}
+                  />
+                </h1>
+                <p>{dataportfolio[3].description}</p>
+                <a href={dataportfolio[3].link}>view project</a>
+              </div>
+            </Col>
+            <Col lg="6" className="d-flex align-items-center justify-content-center">
+              <div className="po_item">
+                <img src={dataportfolio[3].img} alt="" className="portfolio-image" />
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <hr className="t_border my-4 ml-0 text-left" />
+        <br/>
+        <div className="mb-5 po_items_ho">
+          <Row>
+            <Col lg="6" className="d-flex align-items-center justify-content-center">
+              <div className="po_item">
+                <img src={dataportfolio[4].img} alt="" className="portfolio-image" />
+              </div>
+            </Col>
+            <Col lg="6" className="text-center align-self-center">
+              <div className="content text-left">
+              <h2 className="mb-1x">{dataportfolio[4].title}</h2>
+                <h1 className="fluidz-48 mb-1x">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        dataportfolio[4].animated.first,
+                        dataportfolio[4].animated.second,
+                        dataportfolio[4].animated.third,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 40,
+                    }}
+                  />
+                </h1>
+                <p>{dataportfolio[4].description}</p>
+                <a href={dataportfolio[4].link}>view project</a>
+              </div>
+            </Col>
+          </Row>
+        </div>
+
       </Container>
     </HelmetProvider>
   );
