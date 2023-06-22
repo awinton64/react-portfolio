95439a9ed75354f90094a764556d3fafd68cb63e
awinton64 <awinton21@gmail.com>
2023-06-21 23:10:36 -0500

    added legal footer

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Headermain from "../header";
 import "./App.css";
+import { Container, Col, Row } from 'react-bootstrap';
 import { AppProvider } from "../AppContext";
 import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
@@ -17,6 +18,18 @@ export default function App() {
         <Portfolio />
         <About />
         <ContactUs />
+        <Container className="mt-4 px-2">
+          <Row>
+            <Col>
+              <p className="text-center text-secondary">
+                &copy; {new Date().getFullYear()} Andrew Winton. All Rights Reserved.
+              </p>
+              <p className="text-center text-secondary">
+                Made with ❤️.
+              </p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </AppProvider>
   );
